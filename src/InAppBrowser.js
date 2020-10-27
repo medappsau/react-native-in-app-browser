c6b8275807ec0399356fdad97a236a116d6f3c2a
Mike James <mike@medapps.com.au>
2020-10-27 15:10:00 +1100

    init

--- src/InAppBrowser.js
+++ src/InAppBrowser.js
@@ -1,4 +1,11 @@
+import React, { useState, useMemo, useRef} from 'react'
+import styled from 'styled-components/native';
+import urlparse from 'url-parse';
 
+const Header = styled.View``;
+const Footer = styled.View``;
+const Browser = styled.View``;
+const Text = styled.Text``;
 
 export function InAppBrowser({
     url, 
@@ -9,12 +16,31 @@ export function InAppBrowser({
 
     const [currentUrl, setCurrentUrl] = useState(url);
     const initialTitle = useMemo(() => {
+        if (!currentUrl) return '';
         const {hostname} = urlparse(currentUrl);
         setTitle(hostname)
     }, [])
     const [title, setTitle] = useState(initialTitle);
-
+    const webview = useRef()
     return (
-
+        <Modal isOpen={isOpen}>
+            <Header>
+                <Text>{title}</Text>
+            </Header>
+            <Browser>
+                <WebView
+                {...rest}
+                ref={webview}
+                source={{uri: url}} onLoadEnd={event => {
+                    const {nativeEvent} = event;
+                    setCurrentUrl(nativeEvent.url);
+                    setCanGoForward(nativeEvent.canGoForward);
+                    setCanGoBack(nativeEvent.canGoBack);
+                    setTitle(nativeEvent.title);
+                }}/>
+            </Browser> 
+            <Footer>
+            </Footer>
+        </Modal>
     );
 }
